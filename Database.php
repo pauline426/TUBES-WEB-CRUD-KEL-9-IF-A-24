4771bc42858ed742b1fd0760067fa828b723e05d
pajimu02 <pajrimuhamad751@gmail.com>
2025-12-25 22:57:33 +0700

    memperbaiki query database

--- Database.php
+++ Database.php
@@ -127,7 +127,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama_lengkap VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 -- =========================
